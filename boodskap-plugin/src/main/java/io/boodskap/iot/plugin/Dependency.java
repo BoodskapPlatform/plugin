a30e35b818c654150166c1a4d959b718fdd00c63
jegan <jegan@abcs-mbp-4.frontierlocal.net>
2018-11-21 13:59:58 -0600

    Converted to regular class

--- boodskap-plugin/src/main/java/io/boodskap/iot/plugin/Dependency.java
+++ boodskap-plugin/src/main/java/io/boodskap/iot/plugin/Dependency.java
@@ -1,38 +1,103 @@
 package io.boodskap.iot.plugin;
 
+import java.io.Serializable;
+
 /**
- * Annotation to declare maven style jar dependency
+ * Maven style jar dependency
  * 
  * @author Jegan Vincent
  *
  */
-public @interface Dependency {
+public class Dependency implements Serializable {
+	
+	private static final long serialVersionUID = -2802212609296431056L;
 	
 	/**
 	 * Group ID
 	 * @return {@link String}
 	 */
-	String group();
+	private String group;
 	
 	/**
 	 * Artifact ID
 	 * @return {@link String}
 	 */
-	String artifact();
+	private String artifact;
 	
 	/**
 	 * Version
 	 * @return {@link String}
 	 */
-	String version();
+	private String version;
+
+	public Dependency() {		
+	}
+
+	public Dependency(String group, String artifact, String version) {
+		this.group = group;
+		this.artifact = artifact;
+		this.version = version;
+	}
+
+	public String getGroup() {
+		return group;
+	}
+
+	public void setGroup(String group) {
+		this.group = group;
+	}
+
+	public String getArtifact() {
+		return artifact;
+	}
+
+	public void setArtifact(String artifact) {
+		this.artifact = artifact;
+	}
+
+	public String getVersion() {
+		return version;
+	}
+
+	public void setVersion(String version) {
+		this.version = version;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((artifact == null) ? 0 : artifact.hashCode());
+		result = prime * result + ((group == null) ? 0 : group.hashCode());
+		result = prime * result + ((version == null) ? 0 : version.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Dependency other = (Dependency) obj;
+		if (artifact == null) {
+			if (other.artifact != null)
+				return false;
+		} else if (!artifact.equals(other.artifact))
+			return false;
+		if (group == null) {
+			if (other.group != null)
+				return false;
+		} else if (!group.equals(other.group))
+			return false;
+		if (version == null) {
+			if (other.version != null)
+				return false;
+		} else if (!version.equals(other.version))
+			return false;
+		return true;
+	}
 	
-	/**
-	 * Maven jar download link
-	 * <pre>
-	 * Ex: https://search.maven.org/remotecontent?filepath=
-	 * Ex: http://central.maven.org/maven2/
-	 * </pre>
-	 * @return {@link String}
-	 */
-	String repository() default "http://central.maven.org/maven2/";
 }
--- boodskap-plugin/src/main/java/io/boodskap/iot/plugin/PluginDependency.java
+++ boodskap-plugin/src/main/java/io/boodskap/iot/plugin/PluginDependency.java
@@ -1,24 +1,102 @@
 package io.boodskap.iot.plugin;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
 
 /**
- * Annotation to declare all Maven Central Jar dependencies
+ * Maven Central Jar dependencies
  * 
  * @author Jegan Vincent
  *
  */
-@Retention(RetentionPolicy.RUNTIME)
-@Target({ElementType.TYPE})
-public @interface PluginDependency {
+public class PluginDependency implements Serializable {
+	
+	private static final long serialVersionUID = -8501315489317129479L;
+	
+	private List<Dependency> deps = new ArrayList<>();
+	
+	public PluginDependency() {
+	}
+	
+	public static Builder builder() {
+		return new Builder();
+	}
+
+	public List<Dependency> getDeps() {
+		return deps;
+	}
+
+	public void add(Dependency... deps) {
+		this.deps.addAll(Arrays.asList(deps));
+	}
 	
 	/**
-	 * Array of {@link Dependency}
-	 * @return {@link Dependency}
+	 * Method to convert to JSON String
+	 * @return {@link String} in JSON format or null on failure
 	 */
-	Dependency[] dependencies();
+	public String toJson() {
+		try {
+			return JsonUtil.toJSON(this);
+		} catch (JsonProcessingException e) {
+		}
+		return null;
+	}
 	
+	@Override
+	public String toString() {
+		String json = toJson();
+		if(null == json) return super.toString();
+		return json;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((deps == null) ? 0 : deps.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		PluginDependency other = (PluginDependency) obj;
+		if (deps == null) {
+			if (other.deps != null)
+				return false;
+		} else if (!deps.equals(other.deps))
+			return false;
+		return true;
+	}
+	
+	public static class Builder{
+		
+		private final PluginDependency deps = new PluginDependency();
+		
+		private Builder() {
+		}
+		
+		public Builder add(String group, String artifact, String version) {
+			deps.add(new Dependency(group, artifact, version));
+			return this;
+		}
+		
+		public Builder add(Dependency dep) {
+			this.deps.add(dep);
+			return this;
+		}
+		
+		public String build() {
+			return deps.toJson();
+		}
+	}
 }
