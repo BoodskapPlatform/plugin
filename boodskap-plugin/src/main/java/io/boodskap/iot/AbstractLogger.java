5bcb6f2dbce6a54c17c1f81cfaefae8afa322aa4
jeganvicent <jvincent@boodskap.io>
2022-01-20 17:57:03 +0530

    changes

--- boodskap-plugin/src/main/java/io/boodskap/iot/AbstractLogger.java
+++ boodskap-plugin/src/main/java/io/boodskap/iot/AbstractLogger.java
@@ -2,6 +2,8 @@ package io.boodskap.iot;
 
 import org.apache.commons.lang3.exception.ExceptionUtils;
 
+import io.boodskap.iot.plugin.Invokable;
+
 public abstract class AbstractLogger {
 
 	protected abstract void doLogging(String lineInfo, String level, String message);
@@ -14,69 +16,84 @@ public abstract class AbstractLogger {
 		doLogging("", level, String.format(format, args));
 	}
 
-	public void debug(Throwable e) {
-		String message = ExceptionUtils.getStackTrace(e);
-		doLogging("", "debug", message);
+	@Invokable(signature = "void trace(String msg)", help = "")
+	public void trace(String msg) {
+		doLogging("", "trace", msg);
 	}
-	
+
+	@Invokable(signature = "void trace(String format, Object... args)", help = "")
+	public void trace(String format, Object... args) {
+		doLogging("", "trace", String.format(format, args));
+	}
+
+	@Invokable(signature = "void trace(Throwable e)", help = "")
 	public void trace(Throwable e) {
 		String message = ExceptionUtils.getStackTrace(e);
 		doLogging("", "trace", message);
 	}
 	
-	public void trace(String msg) {
-		doLogging("", "trace", msg);
-	}
-
-	public void trace(String format, Object... args) {
-		doLogging("", "trace", String.format(format, args));
-	}
-
+	@Invokable(signature = "void debug(String msg)", help = "")
 	public void debug(String msg) {
 		doLogging("", "debug", msg);
 	}
 
+	@Invokable(signature = "void debug(String format, Object... args)", help = "")
 	public void debug(String format, Object... args) {
 		doLogging("", "debug", String.format(format, args));
 	}
 
+	@Invokable(signature = "void debug(Throwable e)", help = "")
+	public void debug(Throwable e) {
+		String message = ExceptionUtils.getStackTrace(e);
+		doLogging("", "debug", message);
+	}
+	
+	@Invokable(signature = "void info(String msg)", help = "")
+	public void info(String msg) {
+		doLogging("", "info", msg);
+	}
+
+	@Invokable(signature = "void info(String format, Object... args)", help = "")
+	public void info(String format, Object... args) {
+		doLogging("", "info", String.format(format, args));
+	}
+	
+	@Invokable(signature = "void info(Throwable e)", help = "")
 	public void info(Throwable e) {
 		String message = ExceptionUtils.getStackTrace(e);
 		doLogging("", "info", message);
 	}
 	
-	public void info(String msg) {
-		doLogging("", "info", msg);
+	@Invokable(signature = "void warn(String msg)", help = "")
+	public void warn(String msg) {
+		doLogging("", "warn", msg);
 	}
 
-	public void info(String format, Object... args) {
-		doLogging("", "info", String.format(format, args));
+	@Invokable(signature = "void warn(String format, Object... args)", help = "")
+	public void warn(String format, Object... args) {
+		doLogging("", "warn", String.format(format, args));
 	}
 
+	@Invokable(signature = "void warn(Throwable e)", help = "")
 	public void warn(Throwable e) {
 		String message = ExceptionUtils.getStackTrace(e);
 		doLogging("", "warn", message);
 	}
 	
-	public void warn(String msg) {
-		doLogging("", "warn", msg);
+	@Invokable(signature = "void error(String msg)", help = "")
+	public void error(String msg) {
+		doLogging("", "error", msg);
 	}
 
-	public void warn(String format, Object... args) {
-		doLogging("", "warn", String.format(format, args));
+	@Invokable(signature = "void error(String format, Object... args)", help = "")
+	public void error(String format, Object... args) {
+		doLogging("", "error", String.format(format, args));
 	}
 
+	@Invokable(signature = "void error(Throwable e)", help = "")
 	public void error(Throwable e) {
 		String message = ExceptionUtils.getStackTrace(e);
 		doLogging("", "error", message);
 	}
 	
-	public void error(String msg) {
-		doLogging("", "error", msg);
-	}
-
-	public void error(String format, Object... args) {
-		doLogging("", "error", String.format(format, args));
-	}
-
 }
